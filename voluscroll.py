d9871ec546b0da5e884d7dc398359902bb217a07
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2025-08-19 21:56:21 -0400

    Refactor volume scrolling functionality into a class

--- voluscroll.py
+++ voluscroll.py
@@ -1,31 +1,32 @@
 from pynput.keyboard import Controller, Key
-from pynput.mouse import Listener
-
-keyboard_controller = Controller()
-middle_mouse_down = False
+from pynput.mouse import Button, Listener
 
 
-def on_scroll(mouse_x, mouse_y, direction_x, direction_y):
-    print(
-        f"MOUSE X: {mouse_x}, MOUSE Y: {mouse_y}, Direction X: {direction_x}, Direction Y: {direction_y}"
-    )
-    if middle_mouse_down:
+class VolumeScroller:
+    def __init__(self) -> None:
+        self.keyboard_controller = Controller()
+        self.middle_mouse_down = False
+        self.listener = Listener(on_scroll=self.on_scroll, on_click=self.on_click)
+
+    def on_scroll(self, mouse_x, mouse_y, direction_x, direction_y):
+        if not self.middle_mouse_down:
+            return
         if direction_y > 0:
             action = Key.media_volume_up
-        elif direction_y < 0:
+        else:
             action = Key.media_volume_down
-        keyboard_controller.press(action)
+        self.keyboard_controller.press(action)
+
+    def on_click(self, mouse_x, mouse_y, button, pressed):
+        if not button == Button.middle:
+            return
+        self.middle_mouse_down = pressed
+
+    def start(self):
+        # with self.listener:
+        self.listener.start()
 
 
-def on_click(mouse_x, mouse_y, button, pressed):
-    print(
-        f"MOUSE X: {mouse_x}, MOUSE Y: {mouse_y}, Button: {button}, Pressed: {pressed}"
-    )
-    global middle_mouse_down
-    middle_mouse_down = pressed
-
-
-listener = Listener(on_scroll=on_scroll, on_click=on_click)
-
-with listener:
-    listener.join()
+if __name__ == "__main__":
+    volume_scroller = VolumeScroller()
+    volume_scroller.start()
